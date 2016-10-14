b296d7ca203fae5ed8f2202e9b97df65e8fc5e78
hoang <hoangduchuuvn@gmail.com>
2016-10-14 20:27:32 +0700

    vcl with listview

--- app/src/main/java/com/example/hoang/themovie/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/hoang/themovie/adapter/MovieAdapter.java
@@ -1,7 +1,6 @@
 package com.example.hoang.themovie.adapter;
 
 import android.content.Context;
-import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -60,30 +59,30 @@ public class MovieAdapter extends ArrayAdapter<Result> {
             viewHolder = new ViewHolder();
             viewHolder.tvTitile = (TextView) convertView.findViewById(R.id.tvTitle);
             viewHolder.tvOverView = (TextView) convertView.findViewById(R.id.tvOverView);
-            viewHolder.tvOverViewBottom = (TextView) convertView.findViewById(R.id.tvOverViewBottom);
-            viewHolder.tvStar = (TextView) convertView.findViewById(R.id.tvStar);
             viewHolder.ivCover = (ImageView) convertView.findViewById(R.id.ivCover);
-            viewHolder.lilaBg = (LinearLayout)convertView.findViewById(R.id.lilaBg);
-
+            viewHolder.tvStar = (TextView) convertView.findViewById(R.id.tvStar);
+            viewHolder.tvOverViewBottom = (TextView) convertView.findViewById(R.id.tvOverViewBottom);
+            viewHolder.layoutBg = (LinearLayout) convertView.findViewById(R.id.lilaBg);
             convertView.setTag(viewHolder);
         }else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-
         Result movie = getItem(position);
-        viewHolder.tvStar.setText(String.valueOf(movie.getVoteAverage()));
+        double star = movie.getVoteAverage();
+        viewHolder.tvTitile.setText(movie.getTitle());
+        viewHolder.tvStar.setText(String.valueOf(star));
 
-        double rate = movie.getVoteAverage();
-        if(rate > 6){
-            /// vote star > 5 --> set full Background
+
+        if (star >= 6) {
+            viewHolder.tvOverViewBottom.setText(movie.getOverview());
             viewHolder.ivCover.setVisibility(View.GONE);
             viewHolder.tvOverView.setVisibility(View.GONE);
-            viewHolder.tvOverViewBottom.setText(movie.getOverview());
+            /// vote star > 6 --> set full Background
             Picasso.with(getContext()).load("https://image.tmdb.org/t/p/w342" + movie.getPosterPath()).into(new Target() {
                 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
                 @Override
                 public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                    viewHolder.lilaBg.setBackground(new BitmapDrawable(getContext().getResources(), bitmap));
+                    viewHolder.layoutBg.setBackground(new BitmapDrawable(getContext().getResources(), bitmap));
                 }
                 @Override
                 public void onBitmapFailed(Drawable errorDrawable) {
@@ -97,41 +96,105 @@ public class MovieAdapter extends ArrayAdapter<Result> {
             });
             // follow this thread: http://stackoverflow.com/questions/29777354/how-do-i-set-background-image-with-picasso-in-code
             // end vote star > 5 --> set full Background
-        }else{
-            // what else
-            viewHolder.tvOverViewBottom.setVisibility(convertView.GONE);
         }
 
-        //// fill the date
-        viewHolder.tvTitile.setText(movie.getTitle());
-        viewHolder.tvOverView.setText(movie.getOverview());
-
-        // get configuration
-
-        Configuration configuration = getContext().getResources()
-                .getConfiguration();
-        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT){
+        if (star < 6){
+            viewHolder.tvOverViewBottom.setVisibility(View.GONE);
+            viewHolder.tvOverView.setText(movie.getOverview());
             Picasso.with(getContext())
                     .load("https://image.tmdb.org/t/p/w342" + movie.getPosterPath())
                     .placeholder(R.drawable.preloading)
                     .into(viewHolder.ivCover);
-        }else {
-            Picasso.with(getContext())
-                    .load("https://image.tmdb.org/t/p/w342" + movie.getBackdropPath())
-                    .placeholder(R.drawable.preloading)
-                    .into(viewHolder.ivCover);
-
         }
-
         return convertView;
+
+        //// fill the date
+//        viewHolder.tvTitile.setText(movie.getTitle());
+//        viewHolder.tvOverView.setText(movie.getOverview());
+//        viewHolder.tvStar.setText(String.valueOf(movie.getVoteAverage()));
+
+        // get configuration
+
+//        Configuration configuration = getContext().getResources()
+//                .getConfiguration();
+//        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT){
+//            Picasso.with(getContext())
+//                    .load("https://image.tmdb.org/t/p/w342" + movie.getPosterPath())
+//                    .placeholder(R.drawable.preloading)
+//                    .into(viewHolder.ivCover);
+//        }else {
+//            Picasso.with(getContext())
+//                    .load("https://image.tmdb.org/t/p/w342" + movie.getBackdropPath())
+//                    .placeholder(R.drawable.preloading)
+//                    .into(viewHolder.ivCover);
+//
+//        }
+
+
+
     }
+
 
     private class ViewHolder{
         public TextView tvTitile;
-        public TextView tvOverView, tvOverViewBottom, tvStar;
+        public TextView tvOverView;
+        public TextView tvOverViewBottom;
+        public TextView tvStar;
         public ImageView ivCover;
-        public LinearLayout lilaBg;
+        public LinearLayout layoutBg;
     }
 }
 
 
+
+//package com.example.hoang.themovie.adapter;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.TextView;
+//
+//import com.example.hoang.themovie.R;
+//import com.example.hoang.themovie.model.Result;
+//
+//import java.util.List;
+//
+//
+///**
+// * Created by hoang on 10/14/2016.
+// */
+//
+//public class MovieAdapter extends ArrayAdapter<Result> {
+//
+//    public MovieAdapter(Context context, int resource, List<Result> items) {
+//        super(context, resource, items);
+//    }
+//
+//
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//
+//        View view = convertView;
+//        if (view == null) {
+//            LayoutInflater inflater = LayoutInflater.from(getContext());
+//            view =  inflater.inflate(R.layout.item_movie, null);
+//        }
+//        Result p = getItem(position);
+//        if (p != null) {
+//            // Anh xa + Gan gia tri
+//            TextView txttitle = (TextView) view.findViewById(R.id.tvTitle);
+//
+//
+//            /// fill the data
+//            txttitle.setText("abc");
+//
+//
+//
+//        }
+//        return view;
+//    }
+//
+//}
